d65f05de9423e08da58288222a0e7b0fa9549114
Jan Horava <czechinthemail@gmail.com>
2013-05-20 18:09:08 -0700

    improved formatting

--- rts-boxing.js
+++ rts-boxing.js
@@ -6,10 +6,10 @@ function draw(){
         get('buffer').height
     );
 
-    buffer.strokeStyle='#fff';
+    buffer.strokeStyle = '#fff';
 
     /*if mouse down, draw current box*/
-    if(mouse_down===1){
+    if(mouse_down === 1){
         buffer.beginPath();
         buffer.rect(
             mouse_lock_x,
@@ -23,7 +23,7 @@ function draw(){
 
     /*draw saved boxes*/
     i = boxen.length-1;
-    if(i>=0){
+    if(i >= 0){
         do{
             buffer.beginPath();
             buffer.rect(
@@ -52,17 +52,23 @@ function draw(){
         get('canvas').width,
         get('canvas').height
     );
-    canvas.drawImage(get('buffer'),0,0)
+    canvas.drawImage(
+        get('buffer'),
+        0,
+        0
+    )
 }
+
 function get(i){
     return document.getElementById(i)
 }
+
 function resize(){
     get('buffer').width = get('canvas').width = window.innerWidth;
-    get('buffer').height = get('canvas').heigh t= window.innerHeight;
+    get('buffer').height = get('canvas').height = window.innerHeight;
 
-    x = get('canvas').width/2;
-    y = get('canvas').height/2
+    x = get('canvas').width / 2;
+    y = get('canvas').height / 2
 }
 
 var boxen = [];
@@ -84,14 +90,15 @@ setInterval('draw()',30);
 window.onkeydown = function(e){
     i = window.event ? event : e;
     i = i.charCode ? i.charCode : i.keyCode;
-    if(i===27){/*ESC*/
+    if(i === 27){/*ESC*/
         /*delete all boxes*/
         boxen = []
     }
 };
+
 window.onmousedown = function(e){
     e.preventDefault();
-    if(e.button===0){/*Left Click*/
+    if(e.button === 0){/*Left Click*/
         /*start new box*/
         mouse_down = 1;
         mouse_x = e.pageX;
@@ -100,16 +107,18 @@ window.onmousedown = function(e){
         mouse_lock_y = mouse_y
     }
 };
+
 window.onmousemove = function(e){
     /*if mouse is down, update current box*/
-    if(mouse_down>0){
+    if(mouse_down > 0){
         mouse_x = e.pageX;
         mouse_y = e.pageY
     }
 };
+
 window.onmouseup = function(){
     mouse_down = 0;
-    if(mouse_x-mouse_lock_x!=0 || mouse_y-mouse_lock_y!=0){
+    if(mouse_x - mouse_lock_x != 0 || mouse_y - mouse_lock_y != 0){
         /*add current box to array of boxes*/
         boxen.push([
             mouse_lock_x,/*top left x*/
