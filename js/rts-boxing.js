35f2ff4806e7cb70543f6721e531b2fdbc8e658a
honzi <czechinthemail@gmail.com>
2016-06-19 17:09:23 -0700

    replaced input handling with common input.js

--- js/rts-boxing.js
+++ js/rts-boxing.js
@@ -2,13 +2,13 @@
 
 function draw_logic(){
     // If mouse down, draw current unsaved box.
-    if(mouse_down){
+    if(mouse['down']){
         buffer.beginPath();
         buffer.rect(
-          mouse_lock_x,
-          mouse_lock_y,
-          mouse_x - mouse_lock_x,
-          mouse_y - mouse_lock_y
+          mouse['down-x'],
+          mouse['down-y'],
+          mouse['x'] - mouse['down-x'],
+          mouse['y'] - mouse['down-y']
         );
         buffer.closePath();
         buffer.stroke();
@@ -43,74 +43,38 @@ function resize_logic(){
 }
 
 var boxen = [];
-var mouse_down = false;
-var mouse_lock_x = -1;
-var mouse_lock_y = -1;
-var mouse_x = 0;
-var mouse_y = 0;
 
-window.onkeydown = function(e){
-    var key = e.keyCode || e.which;
-
-    // ESC: delete saved boxen.
-    if(key === 27){
-        boxen = [];
-        draw();
-    }
-};
-
-window.onload = init_canvas;
-
-window.onmousedown =
-  window.ontouchstart = function(e){
-    // Only handle left clicks.
-    if(e.button !== 0){
-        return;
-    }
-
-    e.preventDefault();
-
-    mouse_down = true;
-    mouse_x = e.pageX;
-    mouse_y = e.pageY;
-
-    mouse_lock_x = mouse_x;
-    mouse_lock_y = mouse_y;
-};
-
-window.onmousemove =
-  window.ontouchmove = function(e){
-    if(!mouse_down){
-        return;
-    }
-
-    mouse_x = e.pageX;
-    mouse_y = e.pageY;
-
-    draw();
-};
-
-window.onmouseup =
-  window.ontouchcancel =
-  window.ontouchend = function(){
-    mouse_down = false;
-
-    if(mouse_x - mouse_lock_x === 0
-      && mouse_y - mouse_lock_y === 0){
-        return;
-    }
-
-    boxen.push({
-      'height': mouse_y - mouse_lock_y,
-      'width': mouse_x - mouse_lock_x,
-      'x': mouse_lock_x,
-      'y': mouse_lock_y,
-    });
-
-    mouse_lock_x = 0;
-    mouse_lock_y = 0;
-    mouse_x = 0;
-    mouse_y = 0;
-
-    draw();
+window.onload = function(e){
+    init_canvas();
+    init_input(
+      {
+        27: {
+          'todo': function(){
+              boxen = [];
+              draw();
+          },
+        },
+      },
+      {
+        'mousemove': {
+          'todo': function(){
+              if(!mouse['down']){
+                  return;
+              }
+              draw();
+          },
+        },
+        'mouseup': {
+          'todo': function(){
+              boxen.push({
+                'height': mouse['y'] - mouse['down-y'],
+                'width': mouse['x'] - mouse['down-x'],
+                'x': mouse['down-x'],
+                'y': mouse['down-y'],
+              });
+              draw();
+          },
+        },
+      }
+    );
 };
