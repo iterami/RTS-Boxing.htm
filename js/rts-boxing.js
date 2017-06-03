85a54e341129dff2c5f87bde0002c10989067da8
honzi <czechinthemail@gmail.com>
2017-06-03 10:39:45 -0700

    switched core input code to more general event code

--- js/rts-boxing.js
+++ js/rts-boxing.js
@@ -1,35 +1,38 @@
 'use strict';
 
+function draw_boxen(args){
+    canvas_draw_path({
+      'style': 'stroke',
+      'vertices': [
+        {
+          'type': 'rect',
+           'radius': args['radius'],
+           'startAngle': args['startAngle'],
+           'x': args['x'],
+           'y': args['y'],
+         },
+      ],
+    });
+}
+
 function draw_logic(){
     // If mouse down, draw current unsaved box.
-    if(core_input_mouse['down']){
-        canvas_draw_path({
-          'style': 'stroke',
-          'vertices': [
-            {
-              'type': 'rect',
-              'radius': core_input_mouse['x'] - core_input_mouse['down-x'], //x2
-              'startAngle': core_input_mouse['y'] - core_input_mouse['down-y'], //y2
-              'x': core_input_mouse['down-x'],
-              'y': core_input_mouse['down-y'],
-            },
-          ],
+    if(core_mouse['down']){
+        draw_boxen({
+          'radius': core_mouse['x'] - core_mouse['down-x'],
+          'startAngle': core_mouse['y'] - core_mouse['down-y'],
+          'x': core_mouse['down-x'],
+          'y': core_mouse['down-y'],
         });
     }
 
     // Draw saved boxen.
     for(var box in boxen){
-        canvas_draw_path({
-          'style': 'stroke',
-          'vertices': [
-            {
-              'type': 'rect',
-              'radius': boxen[box]['width'], //x2
-              'startAngle': boxen[box]['height'], //y2
-              'x': boxen[box]['x'],
-              'y': boxen[box]['y'],
-            },
-          ],
+        draw_boxen({
+          'radius': boxen[box]['width'],
+          'startAngle': boxen[box]['height'],
+          'x': boxen[box]['x'],
+          'y': boxen[box]['y'],
         });
     }
 
@@ -42,7 +45,7 @@ function draw_logic(){
 }
 
 function repo_init(){
-    core_input_binds_add({
+    core_events_bind({
       'keybinds': {
         27: {
           'todo': function(){
@@ -54,10 +57,10 @@ function repo_init(){
         'mouseup': {
           'todo': function(){
               boxen.push({
-                'height': core_input_mouse['y'] - core_input_mouse['down-y'],
-                'width': core_input_mouse['x'] - core_input_mouse['down-x'],
-                'x': core_input_mouse['down-x'],
-                'y': core_input_mouse['down-y'],
+                'height': core_mouse['y'] - core_mouse['down-y'],
+                'width': core_mouse['x'] - core_mouse['down-x'],
+                'x': core_mouse['down-x'],
+                'y': core_mouse['down-y'],
               });
           },
         },
